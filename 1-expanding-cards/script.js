b51b6a55179a7fff2ae6f3c561fe6aa759a41a7c
Dong Ky <kyhuudong@gmail.com>
2021-03-25 10:51:37 +0700

    laptop push

--- 1-expanding-cards/script.js
+++ 1-expanding-cards/script.js
@@ -3,7 +3,7 @@ const panels = document.querySelectorAll('.panel');
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses();
-        panel.classList.add('active')
+        panel.classList.add('active');
     })
 })
 
