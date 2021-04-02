16278e406de8f693fd06f2891af7fd501c0fa0d3
mrdaragons <kyhuudong@gmail.com>
2021-04-02 10:59:26 +0700

    laptop push

--- 13-random-choices-picker/script.js
+++ 13-random-choices-picker/script.js
@@ -1,14 +1,17 @@
 const tagsEl = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
-textarea.focus()
+textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
+    
     createTags(e.target.value);
 
     if(e.key === 'Enter') {
+
         setTimeout(() => {
-            e.target.value = ''
+            e.target.value = "";
+
         }, 10)
 
         randomSelect();
@@ -16,38 +19,40 @@ textarea.addEventListener('keyup', (e) => {
 
 })
 
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
 
-function createTags(input){
-    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    tagsEl.innerHTML = "";
 
-    tagsEl.innerHTML = ''
     tags.forEach(tag => {
         const tagEl = document.createElement('span');
-        tagEl.classList.add('tag');
+        tagEl.classList.add('tag')
         tagEl.innerText = tag
-        tagsEl.appendChild(tagEl)
+        tagsEl.appendChild(tagEl);
+
     })
+
 }
 
-function randomSelect(){
+function randomSelect() {
+
     const times = 30;
 
-    //This interval run in three seconds 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag();
         highlightTag(randomTag);
 
         setTimeout(() => {
-            unHighlightTag(randomTag)
+            unHighLightTag(randomTag);
         }, 100);
 
-    }, 100)
-    
+    }, 100);
+
     setTimeout(() => {
         clearInterval(interval);
 
-        setTimeout(() => { 
-            const randomTag = pickRandomTag()
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
             highlightTag(randomTag);
         }, 100);
 
@@ -57,14 +62,14 @@ function randomSelect(){
 
 function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
-    return tags[Math.floor(Math.random() * tags.length)]
+    return tags[Math.floor(Math.random() * tags.length)];
+
 }
 
 function highlightTag(tag) {
     tag.classList.add('highlight');
 }
 
-function unHighlightTag(tag) {
+function unHighLightTag(tag) {
     tag.classList.remove('highlight');
 }
-
